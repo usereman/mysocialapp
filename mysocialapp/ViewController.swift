f8716becba7a891a10604bea45d9616b823c1b39
Abdullahhafeez <abdullahhafeez647@gmail.com>
2017-02-20 16:06:32 +0500

    Initial Commit

---
+++ mysocialapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mysocialapp
+//
+//  Created by Abdullah Hafeez on 20/02/2017.
+//  Copyright © 2017 Abdullah Hafeez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
